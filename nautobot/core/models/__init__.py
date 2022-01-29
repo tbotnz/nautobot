1e65da4c72ed6019a124027638213881308e7f8f
jathanism <jathan@gmail.com>
2022-01-28 17:58:31 -0800

    Bi-directional signals and caching happening and working.
    - Not ideal because it's a weird mix of old and new code
    - The map group filtering is incredibly limited but does provide the ability to construct a query filter from an instance that can be used to find NEW eligible group memberships and schedule an update to them.
    - The saving of the group filter is only done in the form save. This needs to be moved to the model if this pattern is going to persist.

--- nautobot/core/models/__init__.py
+++ nautobot/core/models/__init__.py
@@ -2,11 +2,56 @@ import uuid
 
 from django.contrib.contenttypes.fields import GenericRelation
 from django.db import models
+from django.forms.models import model_to_dict
 
 from nautobot.utilities.querysets import RestrictedQuerySet
 
 
-class BaseModel(models.Model):
+class ModelDiffMixin:
+    """
+    A model mixin that tracks model fields' values and provide some useful api
+    to know what fields have been changed.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__initial = self._dict
+
+    @property
+    def diff(self):
+        d1 = self.__initial
+        d2 = self._dict
+        diffs = [(k, (v, d2[k])) for k, v in d1.items() if v != d2[k]]
+        return dict(diffs)
+
+    @property
+    def has_changed(self):
+        return bool(self.diff)
+
+    @property
+    def changed_fields(self):
+        return self.diff.keys()
+
+    def get_field_diff(self, field_name):
+        """
+        Returns a diff for field if it's changed and None otherwise.
+        """
+        return self.diff.get(field_name, None)
+
+    def save(self, *args, **kwargs):
+        """
+        Saves model and set initial state.
+        """
+        super().save(*args, **kwargs)
+        self.__initial = self._dict
+
+    @property
+    def _dict(self):
+        return model_to_dict(self, fields=[field.name for field in
+                             self._meta.fields])
+
+
+class BaseModel(models.Model, ModelDiffMixin):
     """
     Base model class that all models should inhert from.
 
--- nautobot/extras/models/groups.py
+++ nautobot/extras/models/groups.py
@@ -1,5 +1,6 @@
 """Dynamic Groups Models."""
 
+import logging
 import uuid
 
 from django.conf import settings
@@ -8,8 +9,7 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.core.exceptions import ValidationError
 from django.core.serializers.json import DjangoJSONEncoder
-from django.db import models
-from django.db.models import Model
+from django.db import models, transaction
 
 from nautobot.extras.models import ChangeLoggedModel
 from nautobot.core.models import BaseModel
@@ -24,7 +24,7 @@ class DynamicGroupQuerySet(RestrictedQuerySet):
         """
         Return all `DynamicGroup` assigned to the given object.
         """
-        if not isinstance(obj, Model):
+        if not isinstance(obj, models.Model):
             raise TypeError(f"{obj} is not an instance of Django Model class")
 
         # Check if dynamicgroup is supported for this model
@@ -51,6 +51,13 @@ class DynamicGroup(BaseModel, ChangeLoggedModel):
         verbose_name="Object Type",
         help_text="The type of object for this group.",
     )
+    _members_cache = models.JSONField(
+        encoder=DjangoJSONEncoder,
+        blank=True,
+        editable=False,
+        default=list,
+    )
+    _members_count = models.PositiveIntegerField(editable=False, blank=True, default=0)
 
     filter = models.JSONField(
         encoder=DjangoJSONEncoder,
@@ -94,9 +101,11 @@ class DynamicGroup(BaseModel, ChangeLoggedModel):
         dynamicgroupmap_class = get_dynamicgroupmap_for_model(model)
         return dynamicgroupmap_class.get_queryset(self.filter)
 
+    @property
     def count(self):
         """Return the number of objects in the group."""
-        return self.get_queryset().count()
+        # return self.get_queryset().count()
+        return self._members_count
 
     def get_dynamicgroup_url(self):
         """Get url to group members."""
@@ -123,18 +132,51 @@ class DynamicGroup(BaseModel, ChangeLoggedModel):
 
         return self._map
 
+    def _purge_assignments(self):
+        self.dg_assignments.all().delete()
+        self.clean_assignments()
+
+    def clean_assignments(self):
+        member_pks = self.dg_assignments.values_list("object_id", flat=True)
+        # self._members_cache = json.dumps(assignments, cls=DjangoJSONEncoder)
+        self._members_cache = [str(m) for m in member_pks]
+        self.clean_count()
+
+    def clean_count(self):
+        count = self.dg_assignments.count()
+        self._members_count = count
+
+    def _assign_member(self, member):
+        """Assign an object to this DynamicGroup."""
+        dga = DynamicGroupAssignment(
+            dynamic_group=self,
+            content_object=member,
+        )
+        dga.full_clean()
+        return dga
+
+    @transaction.atomic
+    def assign_members(self, members):
+        """Assign a list of members to this DynamicGroup."""
+        # First purge existing assignments.
+        self._purge_assignments()
+
+        # Then create the new ones.
+        new_members = []
+        for member in members:
+            new_members.append(self._assign_member(member))
+
+        # Assert that assignments are cached and the count is updated.
+        # self.clean_assignments()
+
+        return new_members
+
     def save(self, **kwargs):
         members = self.get_queryset()
-        if members.exists():
-            for obj in members.iterator():
-                DynamicGroupAssignment.objects.get_or_create(
-                    content_type=self.content_type,
-                    object_id=obj.pk,
-                )
-
-
-
-
+        new_members = self.assign_members(members)
+        DynamicGroupAssignment.objects.bulk_create(new_members)
+        self.clean_assignments()  # Cache new assignments
+        super().save(**kwargs)
 
 
 class DynamicGroupAssignment(ChangeLoggedModel, BaseModel):
@@ -160,10 +202,77 @@ class DynamicGroupAssignment(ChangeLoggedModel, BaseModel):
     )
 
     def clean(self):
-        if self.content_type != dynamic_group.content_type:
+        if self.content_type != self.dynamic_group.content_type:
             raise ValidationError({
                 "content_type": "Object content_type must match that of the DynamicGroup to which it is assigned"
             })
 
     def __str__(self):
         return f"{self.content_object!r} -> {self.dynamic_group!r}"
+
+
+from django.dispatch import receiver
+from django.db.models import signals
+
+logger = logging.getLogger(__name__)
+
+@receiver(signals.post_init)
+def stash_existing_dynamic_group_memberships(sender, instance, **kwargs):
+    if sender == DynamicGroupAssignment:
+        return
+
+    skip = ["example_plugin"]
+    if instance._meta.app_label in skip:
+        return
+
+    if hasattr(instance, "dynamic_groups"):
+        logger.debug("Instance %r about to be saved/deleted; stashing Dynamic Groups", instance)
+        instance._existing_dynamic_groups = list(
+            instance.dynamic_groups.all()
+            # instance.dynamic_groups.select_related("dynamic_group")
+        )
+
+
+@receiver(signals.post_save)
+@receiver(signals.post_delete)
+def refresh_dynamic_group_membership(sender, instance, created=False, raw=False, **kwargs):
+    # Only make changes if this isn't raw (e.g. during fixture loading)
+    if raw:
+        return
+
+    if sender == DynamicGroupAssignment:
+        return
+
+    logger.debug("Instance %r saved/deleted", instance)
+
+    # Enumerate the NEW assignments for the instance and call `save()` on each
+    # DynamicGroup
+    groups_to_update = set()
+    if hasattr(instance, "dynamic_groups"):
+        for dga in instance.dynamic_groups.select_related("dynamic_group"):
+            logger.debug("Refreshing NEW dynamic group %r", dga)
+            groups_to_update.add(dga.dynamic_group)
+            # dga.dynamic_group.save()
+
+    # Enumerate the OLD assignments for the instance and call `save()` on each
+    # DynamicGroup
+    if hasattr(instance, "_existing_dynamic_groups"):
+        for dga in instance._existing_dynamic_groups:
+            logger.debug("Refreshing OLD dynamic group %r", dga)
+            groups_to_update.add(dga.dynamic_group)
+            # dga.dynamic_group.save()
+
+    # Call `save()` on the ones we need to update
+    for group in groups_to_update:
+        group.save()
+
+    # Find DynamicGroups for which this instance is now eligible, and update
+    # them.
+    from nautobot.utilities.utils import get_dynamicgroupmap_for_model
+    map_class = get_dynamicgroupmap_for_model(instance._meta.model)
+    if map_class is not None:
+        new_filter = map_class.get_queryset_filter(instance)
+        new_groups = DynamicGroup.objects.filter(new_filter)
+        for new_group in new_groups:
+            logger.debug("Refreshing NEW dynamic group %r", new_group)
+            new_group.save()
